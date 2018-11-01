ad8cb0c03f8e8361914a54fccb9ea580618b4034
David <davidzapata56@gmail.com>
2018-11-01 09:22:08 -0500

    localhostnombre

---
+++ app.rb
@@ -0,0 +1,31 @@
+# crear un aplicación Web usando Sinatra que reciba un parámetro llamado
+# nombre en
+# la URL y muestre un mensaje de bienvenida con el valor de ese parámetro.
+# Por ejemplo, si ingresas a
+# http://localhost:4567/?nombre=Juan
+# la aplicación debe mostrar Hola Juan! dentro de una etiqueta h1.
+# Si no recibes el query string parameter, o si viene vacío, deberás
+# mostrar la frase Hola desconocido!.
+
+
+
+
+
+
+
+
+
+require "sinatra"
+
+get '/' do
+  unless params[:nombre]
+    "Hola desconocido"
+  else
+    "<h1>Hola #{params[:nombre]}</h1>"
+  end
+
+end
+
+get '/makers/:nombre' do
+  "<h1> Hola #{params[:nombre].capitalize}</h1>"
+end
